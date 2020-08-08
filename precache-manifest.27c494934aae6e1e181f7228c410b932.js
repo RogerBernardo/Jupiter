f1ac15895251070d01f8219fa5b96722b8d55c6f
Roger Bernardo de Melo Lima <rbmelolima@gmail.com>
2020-08-08 15:19:57 -0300

    Updates

---
+++ precache-manifest.27c494934aae6e1e181f7228c410b932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d233573a939e3a3fa1983eb2b70f490",
+    "url": "/Atena/index.html"
+  },
+  {
+    "revision": "723c84bccf52b38b9833",
+    "url": "/Atena/static/css/main.a23adf24.chunk.css"
+  },
+  {
+    "revision": "dec80f747aae5b5050e6",
+    "url": "/Atena/static/js/2.bdd9e563.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Atena/static/js/2.bdd9e563.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723c84bccf52b38b9833",
+    "url": "/Atena/static/js/main.bac37cb6.chunk.js"
+  },
+  {
+    "revision": "359bb11cc8574c40f0f9",
+    "url": "/Atena/static/js/runtime-main.4c4d6efa.js"
+  },
+  {
+    "revision": "b8f2b9600a9b30e8857db44c89bee465",
+    "url": "/Atena/static/media/owl.b8f2b960.svg"
+  }
+]);
